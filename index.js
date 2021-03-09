a775f7453f4706ebe9968f0b2be37919a3bf5970
santhosh-ponnusamy <chaku41@gmail.com>
2021-03-09 21:37:04 +0530

    change

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-
-var SharedContext = require('@celzero/globalcontext').SharedContext
-var SingleRequest = require('@celzero/single-request').SingleRequest
-var UserOperation = require('@celzero/free-useroperation').UserOperation
-var DnsWork = require("@celzero/dns-blocker").DnsWork
+var SharedContext = require('@serverless-dns/globalcontext').SharedContext
+var SingleRequest = require('@serverless-dns/single-request').SingleRequest
+var Modules = require("@serverless-dns/free-user").Modules
 
 addEventListener('fetch', event => {
 	event.respondWith(handleRequest(event))
@@ -14,34 +12,32 @@ async function handleRequest(event) {
 
 
 let commonContext = new SharedContext()
-let DnsOperation = new DnsWork()
 
 async function proxyRequest(event) {
-	let thisRequest= new SingleRequest()
-
-	var request = event.request
+	let thisRequest = new SingleRequest()
 	let res
-
-	let hook = new Hooks()
 	try {
-
-		if (request.method === "OPTIONS") {
+		if (event.request.method === "OPTIONS") {
 			res = new Response()
 			res.headers.set('Content-Type', 'application/json')
 			res.headers.set('Access-Control-Allow-Origin', '*')
 			res.headers.set('Access-Control-Allow-Headers', '*')
 			return res
 		}
-
-		hook.register(LoadGlobalContext, [commonContext,thisRequest])
-		hook.register(Command, [commonContext, event, thisRequest])
-		hook.register(LoadSingleRequest, [commonContext, event,thisRequest])
-		hook.register(LoadCurrentUserConfig, [commonContext, event, thisRequest])
-		hook.register(CheckRequestBlock, [commonContext, thisRequest])
-		hook.register(ResolveDns, [thisRequest])
-		hook.register(CheckCnameBlock, [commonContext, event, thisRequest])
-		await hook.call()
-
+		let Caller
+		for (let i = 0; i <= Modules.length - 1; i++) {
+			Caller = new Modules[i]()
+			await Caller.RethinkModule(commonContext, thisRequest, event)
+			if (thisRequest.StopProcessing) {
+				if (thisRequest.IsException) {
+					thisRequest.DnsExceptionResponse()
+				}
+				else if (thisRequest.IsInvalidFlagBlock == true) {
+					thisRequest.CustomResponse("Invalid Flag", "User Invalid Flag Block")
+				}
+				break
+			}
+		}
 	}
 	catch (e) {
 		//thisRequest.exception = e
@@ -56,91 +52,6 @@ async function proxyRequest(event) {
 		res.headers.delete('cf-ray')
 		return res
 	}
-
+	
 	return thisRequest.httpResponse
-}
-
-async function Command(commonContext, event, thisRequest) {
-	if (event.request.method === "GET") {
-		commonContext.CommandOperation(event.request.url, thisRequest)
-	}
-	return thisRequest
-}
-
-async function LoadGlobalContext(commonContext, thisRequest) {
-	if (commonContext.loaded == false) {
-		await commonContext.Init(thisRequest)
-	}
-	return thisRequest
-}
-
-async function LoadSingleRequest(commonContext, event, thisRequest) {
-	let retryCount = 0;
-	let retryLimit = 5;
-	while (commonContext.loaded == false) {
-		if (retryCount >= retryLimit) {
-			break
-		}
-		await sleep(10)
-		retryCount++
-	}
-	if (commonContext.loaded == true) {
-		await thisRequest.Init(event, commonContext)
-	}
-	return thisRequest
-}
-
-async function LoadCurrentUserConfig(commonContext, event, thisRequest) {
-	let userWork = new UserOperation()
-	userWork.LoadUser(thisRequest, commonContext, event)
-	return thisRequest
-}
-
-
-async function ResolveDns(thisRequest) {
-	await DnsOperation.ResolveDns(thisRequest)
-	return thisRequest
-}
-
-async function CheckCnameBlock(commonContext, event, thisRequest) {
-	await DnsOperation.CheckResponseCnameDnsBlock(thisRequest, commonContext, event)
-	return thisRequest
-}
-
-
-async function CheckRequestBlock(commonContext, thisRequest) {
-	DnsOperation.CheckDnsBlock(thisRequest, commonContext)
-	return thisRequest
-}
-
-const sleep = ms => {
-	return new Promise(resolve => {
-		setTimeout(resolve, ms);
-	});
-};
-
-class Hooks {
-	constructor() {
-		this.hook = []
-	}
-
-	register(func, args) {
-		this.hook.push({ "func": func, "args": args })
-	}
-
-	async call() {
-		let response
-		for (let i = 0; i <= this.hook.length - 1; i++) {
-			response = await this.hook[i].func(...this.hook[i].args)
-			if (response.StopProcessing) {
-				if (response.IsException) {
-					response.DnsExceptionResponse()
-				}
-				else if (response.IsDnsBlock == true || response.IsCnameDnsBlock == true) {
-					response.DnsBlockResponse()
-				}
-				break
-			}
-		}
-	}
 }
